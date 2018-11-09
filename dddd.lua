e76d2d24d39a5b4421557807c3009f47faa259bf
Sergo733 <44901320+Sergo733@users.noreply.github.com>
2018-11-09 20:11:23 +0400

    Add files via upload

--- dddd.lua
+++ dddd.lua
@@ -7,8 +7,8 @@ local mainContainer, window = MineOSInterface.addWindow(GUI.titledWindow(50, 30,
 window.backgroundPanel.colors.transparency = 0.2
 
 
-window:addChild(GUI.label(17, 10, window.width, window.height, 0x5A5A5A, "Hello,World! 2 (Windowed Edition(Fixed))"))
-window:addChild(GUI.label(2, 26, window.width, window.height, 0x5A5A5A, "Version 1.00"))
+window:addChild(GUI.label(18, 10, window.width, window.height, 0x5A5A5A, "Hello,World! 2 (Windowed Edition(Fixed))"))
+window:addChild(GUI.label(2, 26, window.width, window.height, 0x5A5A5A, "Version 1.10"))
 window:addChild(GUI.label(48, 26, window.width, window.height, 0x5A5A5A, "by Sergo73,2018 MIT"))
 
 window:addChild(GUI.image(20, 14, image.load("/ssss.pic")))
